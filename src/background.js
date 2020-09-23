2593dabb035841e86fa754c84f1f1997f115daec
Jacopo Jannone <jacopo.jannone@jacopojannone.com>
2020-09-23 21:07:03 +0200

    Fix #2

--- src/background.js
+++ src/background.js
@@ -1,7 +1,7 @@
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         if (request.fetchJson) {
-            fetch(request.fetchJson)
+            fetch(request.fetchJson, {headers: {"appFrom": "pb"}})
                 .then(response => response.json())
                 .then(response => sendResponse(response));
             return true;
